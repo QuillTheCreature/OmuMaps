e648806a23648b4aceac008e8724105810cf6e04
Quill <292788791+QuillTheCreature@users.noreply.github.com>
2026-06-17 10:28:33 +0200

    Fixed list.json name and made assets path relative

--- build.sh
+++ build.sh
@@ -16,4 +16,4 @@ while IFS= read -r map; do
     jq 'walk(if type == "object" then with_entries(.key |= (.[0:1] | ascii_downcase) + .[1:] | if .key == "extent" and (.value | type) == "object" then .value = {a: {x: .value.x1, y: .value.y1}, b: {x: .value.x2, y: .value.y2}} elif .key == "url" and (.value | type) == "string" then .value |= "maps/" + . else . end) else . end)' "$outputpath/$map/map.backup.json" > "$outputpath/$map/map.json"
 done < <(yq '.[] | select(.id == "DefaultMapPool") | .maps[]' "$mapyaml")
 
-printf '%s\n' "${processedMaps[@]}" | jq -R . | jq -s 'map({name: ., id: .}) | {maps: .}' > "$outputpath/maps.json"
+printf '%s\n' "${processedMaps[@]}" | jq -R . | jq -s 'map({name: ., id: .}) | {maps: .}' > "$outputpath/list.json"
